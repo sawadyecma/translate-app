fcc14f5ff283cb76aa696dbe3972e4c6fb3c9603
sawadyecma <snowslidez@yahoo.co.jp>
2022-08-06 00:03:25 +0900

    add kind to Pair Component

--- svelte-app/src/lib/Pairs.svelte
+++ svelte-app/src/lib/Pairs.svelte
@@ -1,14 +1,44 @@
 <script lang="ts">
+  import Button, { Group, Label } from "@smui/button";
   export let pairs: { en: string; ja: string }[];
+  let kind: "onlyEn" | "vertical" | "horizontal" = "onlyEn";
 </script>
 
 <div>
-  <ul>
-    {#each pairs as pair}
-      <li>
-        <p>{pair.en}</p>
-        <p>{pair.ja}</p>
-      </li>
-    {/each}
-  </ul>
+  <Group variant="raised">
+    <Button on:click={() => (kind = "onlyEn")} variant="raised">
+      <Label>英語だけ</Label>
+    </Button>
+    <Button on:click={() => (kind = "vertical")} variant="raised">
+      <Label>縦並び</Label>
+    </Button>
+    <Button on:click={() => (kind = "horizontal")} variant="raised">
+      <Label>横並び</Label>
+    </Button>
+  </Group>
+
+  {#if kind === "horizontal"}
+    <table>
+      {#each pairs as pair}
+        <tr>
+          <td style="border-bottom: 1px solid gray">{pair.en}</td>
+          <td style="border-bottom: 1px solid gray">{pair.ja}</td>
+        </tr>
+      {/each}
+    </table>
+  {:else}
+    <ul style="list-style-type: none;">
+      {#each pairs as pair}
+        <li>
+          <p style="height: 0.8rem;">{pair.en}</p>
+          <span style="height: 0.5rem;color: gray;font-size: 0.5rem">
+            {#if kind !== "onlyEn"}
+              {pair.ja}
+            {/if}
+          </span>
+        </li>
+        <hr style="height:0.5px" />
+      {/each}
+    </ul>
+  {/if}
 </div>
